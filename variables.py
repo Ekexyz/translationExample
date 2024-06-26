d137227787818d738841637f562e9cb5fccaeda6
Robert Striekwold <rstriekwold@copado.com>
2024-06-26 10:05:03 +0200

    test

--- variables.py
+++ variables.py
@@ -9,13 +9,13 @@ NL = {'URL': 'https://www.airbnb.nl/',
 
 
 ENG =  {'URL': 'https://www.airbnb.com/',
-    'cookies_Text': 'We gebruiken cookies',
-    'cookies_Button': 'Oké',
-    'destination_field_Label': 'Waar',
-    'destination_field_Value': 'Amsterdam',
-    'click_destination_Text': 'Amsterdam, Netherlands',
-    'search_Button': 'Zoeken',
-    'result_Text': 'Meer dan 1000 accommodaties in Amsterdam' }
+    'cookies_Text': 'We use Cookies',
+    'cookies_Button': 'OK',
+    'destination_field_Label': 'Where',
+    'destination_field_Value': 'London',
+    'click_destination_Text': 'London, United Kingdom',
+    'search_Button': 'Search',
+    'result_Text': 'Over 1,000 places in London' }
 
 def get_variables(env):
     if env == 'NL':
