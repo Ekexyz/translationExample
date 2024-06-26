00c284cf2cc9df792067acdebc38e30b171dea0f
Robert Striekwold <rstriekwold@copado.com>
2024-06-26 10:01:53 +0200

    test

--- variables.py
+++ variables.py
@@ -8,9 +8,14 @@ NL = {'URL': 'https://www.airbnb.nl/',
     'result_Text': 'Meer dan 1000 accommodaties in Amsterdam' }
 
 
-uat = {'scalar' : 'Some other value',
-              'LIST__list': ['Some','other','value'],
-              'extra': 'variables1 does not have this at all'}
+uat =  {'URL': 'https://www.airbnb.com/',
+    'cookies_Text': 'We gebruiken cookies',
+    'cookies_Button': 'Oké',
+    'destination_field_Label': 'Waar',
+    'destination_field_Value': 'Amsterdam',
+    'click_destination_Text': 'Amsterdam, Netherlands',
+    'search_Button': 'Zoeken',
+    'result_Text': 'Meer dan 1000 accommodaties in Amsterdam' }
 
 def get_variables(env):
     if env == 'NL':
