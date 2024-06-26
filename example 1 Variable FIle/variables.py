1a927ef5b64d64e99a7c2b5735185ea5621df8b3
Robert Striekwold <rstriekwold@copado.com>
2024-06-26 10:43:56 +0200

    test

---
+++ example 1 Variable FIle/variables.py
@@ -0,0 +1,27 @@
+NL = {'URL': 'https://www.airbnb.nl/',
+    'cookies_Text': 'We gebruiken cookies',
+    'cookies_Button': 'Oké',
+    'destination_field_Label': 'Waar',
+    'destination_field_Value': 'Amsterdam',
+    'click_destination_Text': 'Amsterdam, Netherlands',
+    'search_Button': 'Zoeken',
+    'result_Text': 'Meer dan 1000 accommodaties in Amsterdam' }
+
+
+ENG =  {'URL': 'https://www.airbnb.com/',
+    'cookies_Text': 'We use cookies',
+    'cookies_Button': 'OK',
+    'destination_field_Label': 'Where',
+    'destination_field_Value': 'London',
+    'click_destination_Text': 'London, United Kingdom',
+    'search_Button': 'Search',
+    'result_Text': 'Over 1,000 places in London' }
+
+def get_variables(env):
+    if env == 'NL':
+        return NL
+    elif env == 'ENG':
+        return ENG
+    else:
+        # default variables
+        return NL
